2cd9b0cc56d2b2685b1be8b84831fb3731640b90
Sebas0616 <56145100+Sebas0616@users.noreply.github.com>
2020-02-24 17:19:31 -0500

    Delete Factorial.py

--- Ejercicios Modelos II/Factorial.py
+++
@@ -1,9 +0,0 @@
-n = float(input("Ingrese el número del cual desea saber su factorial: "))
-
-def fact(n):
-    if(n<=1):
-        return 1
-    else:
-        return n*fact(n-1)
-
-print("El factorial de " + str(n) + " es: " + str(fact(n)))
